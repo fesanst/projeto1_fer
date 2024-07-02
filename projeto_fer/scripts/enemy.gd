a0a7674b40944ff7a3a2cc25e0872e66fa4c0989
nanda <130380716+fesanst@users.noreply.github.com>
2024-07-01 22:37:15 -0300

    01/07 ferr

---
+++ projeto_fer/scripts/enemy.gd
@@ -0,0 +1,17 @@
+extends RigidBody2D
+
+@onready var anim = $anim
+
+func _ready():
+	var enemy_types = anim.sprite_frames.get_animation_names()
+	anim.play(enemy_types[randi() % enemy_types.size()])
+	print(enemy_types)
+
+func _process(delta):
+	pass
+
+
+
+func _on_visible_screen_exited():
+	queue_free()
+
